7e81a5f7af951e92f03386ed745b4c903cb436a8
Lejla <37418679+Lekicaa@users.noreply.github.com>
2023-05-21 20:44:29 +0200

    POstavljen sidro na headeru

--- index.js
+++ index.js
@@ -7,3 +7,12 @@
   open.addEventListener("click",()=>{  
     menu.style.visibility="visible";  
   })
+
+$('.js-anchor-link').click(function(e){
+  e.preventDefault();
+  var target = $($(this).attr('href'));
+  if(target.length){
+    var scrollTo = target.offset().top;
+    $('body, html').animate({scrollTop: scrollTo+'px'}, 800);
+  }
+});
